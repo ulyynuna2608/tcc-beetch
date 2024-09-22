09c76f5febc53aad24fa04129ad938799bb706f9
Nic-Antonini <nicki.pablo123@gmail.com>
2024-09-22 20:06:58 -0300

    22/09/24
    - troquei os dados do banco de dados em 'connection.js', para poder conectar no banco de dados do meu notebook aqui de casa;
    
    -Tirei arquivos como favicon.ico, page.js, etc. Pois são arquivos de um projeto em next e não precisamos de next.
    
    -O npm install não foi feito ainda, com os módulos corretos, como pedido pelo professor Everton no documento dele de node.js.

--- database/connection.js
+++ database/connection.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql2/promise'); 
 
-const bd_usuario = 'us_etim_122_g1'; // usuário
-const bd_senha = 'ab1506'; // senha
-const bd_servidor = '10.67.22.216'; // servidor
+const bd_usuario = 'root'; // usuário
+const bd_senha = 'bdnicolas'; // senha
+const bd_servidor = '127.0.0.1'; // servidor
 const bd_porta = '3306'; // porta
 const bd_banco = 'BeeTech'; // nome do banco
 let connection;
     
 const config = {
-    host: bd_servidor, 
+    host: bd_servidor,  
     port: bd_porta, //Default: 3306
     user: bd_usuario, 
     password: bd_senha, 
